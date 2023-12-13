f5cbef6a7c399d8dfbddf3de15299506669dc0be
Andreas Fink <andreas@fink.org>
2023-12-13 12:47:07 +0100

    wip

--- ulibcamel/ulibcamel_config.h
+++
@@ -1,3 +0,0 @@
-/* ulibcamel/ulibcamel_config.h.  Generated from ulibcamel_config.h.in by configure.  */
-/* ulibcamel_config.h */
-
--- ulibcamel_config.h
+++
@@ -1,2 +0,0 @@
-/* ulibcamel_config.h.  Generated from ulibcamel_config.h.in by configure.  */
-/* ulibcamel_config.h */
